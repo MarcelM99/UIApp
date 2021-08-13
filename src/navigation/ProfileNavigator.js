6c36748b7c3690428d2a761e7111ec7e21b094b4
Marc Marcel <lemonv2_mnx@yahoo.com>
2021-08-13 13:00:56 +0300

    no message

---
+++ src/navigation/ProfileNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import SettingsScreen from '../screens/SettingsScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+
+const Stack = createStackNavigator();
+
+const ProfileNavigator = (props) => {
+    return (
+        <Stack.Navigator initialRouteName={props.route.params===undefined?'Profile':props.route.params.screeName} screenOptions={{headerMode:false}}>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+    )
+}
+
+export default ProfileNavigator;
